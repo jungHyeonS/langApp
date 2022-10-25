0e64bee7f9b4cae3143968a6a2293fd9b99bec62
jungHyeonS <fbtls25@naver.com>
2022-10-25 22:37:18 +0900

    react-native : more interpolations

--- App.js
+++ App.js
@@ -19,29 +19,32 @@ height: 200px;
 
 export default function App() {
   const [up,setUp] = useState(false);
-  const Y_POSITION = useRef(new Animated.Value(300)).current
+  const POSITION = useRef(new Animated.ValueXY({x:0,y:300})).current
   const toggleUp = () => setUp((prev) => !prev)
   const moveUp = () => {
-    Animated.timing(Y_POSITION,{
+    Animated.timing(POSITION,{
       toValue : up ? 300 : -300,
-      useNativeDriver:true,
-      duration:5000
+      useNativeDriver:false,
     }).start(toggleUp)
   }
-  const opacity = Y_POSITION.interpolate({
-    inputRange: [-300,0,300],
-    outputRange : [1,0.5,1]
-  })
-  const borderRadius = Y_POSITION.interpolate({
+  const borderRadius = POSITION.y.interpolate({
     inputRange : [-300,300],
     outputRange : [100,0]
+  })
+  const rotation = POSITION.y.interpolate({
+    inputRange:[-300,300],
+    outputRange:["-360deg","360deg"]
+  })
+  const bgColor = POSITION.y.interpolate({
+    inputRange:[-300,300],
+    outputRange:["rgb(255,99,71)","rgb(71,166,255)"]
   })
   return (
     <Container>
         <Box style={{
-          opacity,
           borderRadius,
-          transform : [{translateY:Y_POSITION}]
+          backgroundColor:bgColor,
+          transform : [{rotateY:rotation},{translateY:POSITION.y}]
         }} onPress={moveUp}/>
     </Container>
   );
